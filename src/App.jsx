76feb2b04b9947b4d6fb85d57577ede36723491a
easmmava <easmmava@gmail.com>
2024-02-25 13:22:39 +0330

    commit 6

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,24 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [characters, setCharacters] = useState(allCharacters);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
-      .then((data) => setCharacters(data.results.slice(0,4)));
+      .then((data) => {
+        setCharacters(data.results.slice(0, 4));
+      });
+    setIsLoading(false);
   }, []);
 
 
   return (
     <div className="app">
-      <Navbar numOfResult={characters.length}/>
+      <Navbar numOfResult={characters.length} />
       <div className="main">
-        <CharacterList characters={characters}/>
+        <CharacterList characters={characters} isLoading={isLoading} />
         <CharacterDetail />
       </div>
     </div>
---
+++ src/components/Loader.jsx
@@ -0,0 +1,5 @@
+function Loader() {
+  return <div>Loading Data...</div>;
+}
+
+export default Loader;
